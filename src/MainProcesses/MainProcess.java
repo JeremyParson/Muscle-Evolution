7658c77f21ded6a69482057149b1700893f60d3a
Jeremy Parson <Jsbparson@gmail.com>
2018-07-03 16:54:02 -0400

    first commit

---
+++ src/MainProcesses/MainProcess.java
@@ -0,0 +1,12 @@
+package MainProcesses;
+
+import org.dyn4j.dynamics.joint.DistanceJoint;
+
+public class MainProcess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
